edfe1992d00fe3bff40688c8b66d85c89a24e6cb
Maciej <raniszem@gmail.com>
2026-04-12 19:04:20 +0200

    FindComponentByClass learning

--- Source/DungeonEscape/TriggerComponent.cpp
+++ Source/DungeonEscape/TriggerComponent.cpp
@@ -6,19 +6,35 @@
 UTriggerComponent::UTriggerComponent()
 {
 	PrimaryComponentTick.bCanEverTick = true;
-	UE_LOG(LogTemp, Display, TEXT("TriggerComponent::UTriggerComponent"));
 }
 
 void UTriggerComponent::BeginPlay()
 {
 	Super::BeginPlay();
-	UE_LOG(LogTemp, Display, TEXT("%s works"), *GetOwner()->GetName());
+	if (MoverActor != nullptr)
+	{
+		Mover = MoverActor->FindComponentByClass<UMover>();
+		if (Mover != nullptr)
+		{
+			UE_LOG(LogTemp, Display, TEXT("Succesfuly found mover component"));
+			Mover->ShouldMove = true;
+
+		}
+		else
+		{
+			UE_LOG(LogTemp, Display, TEXT("Failed to find mover component"));
+
+		}
+	}
+	else
+	{
+		UE_LOG(LogTemp, Display, TEXT("Mover is nullptr"));
+	}
 }
 
 void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType,
 	FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-	UE_LOG(LogTemp, Display, TEXT("TriggerComponent is ticking"));
 
 }
